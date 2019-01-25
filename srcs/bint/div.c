0e9a4ece4c68561bd746fdcc83302f4414cf93cd
Arthur Baurens <elementscitiessuport@gmail.com>
2019-01-25 20:40:26 +0100

    started knuth's division

--- srcs/bint/div.c
+++ srcs/bint/div.c
@@ -6,7 +6,7 @@
 /*   By: abaurens <abaurens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/20 18:25:03 by abaurens          #+#    #+#             */
-/*   Updated: 2019/01/24 20:49:36 by abaurens         ###   ########.fr       */
+/*   Updated: 2019/01/25 00:33:48 by abaurens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,6 +50,218 @@ unsigned int	bidiv_maxq9(t_bint *n1, t_bint *n2)
 		bisub(&m, &m, n2);
 	*n1 = m;
 	return (r);
+}
+
+/*
+{x} note le plus grand entier inférieur ou égal à x (arrondir à l'inférieur || simple cast)
+
+u = dividende (n1).
+v = diviseur (n2).
+q = res.
+
+m + n = longueur du dividende (n1)
+n = longueur du diviseur (n2)
+
+m = longueur du dividende - longueur du diviseur
+
+Multiplier u et v par d = {b/(v[n-1]+1]}
+(c'est une étape de normalisation, l'important dans le choix
+de d est que v[n-1] soit plus grand ou égal à B/2 à la fin)
+
+Pour j de m à 0 par pas de -1
+   qh = {(u[j+n]B + u[j+n-1])/v[n-1]}
+   rh = (u[j+n]B+u[j+n-1]) mod v[n-1]
+   Si qh = B ou qh*v[n-2] > rh*B + u[j+n-2]
+      qh = qh-1
+      rh = rh +v[n-1]
+      si rh < B et qh*v[n-2] > rh*B + u[j+n-2]
+         qh = qh -1
+         rh = rh + v[n-1]
+      fin si
+   fin si
+   u = u - qh v B^j
+   si u < 0
+      u = u + v
+      qh = qh - 1
+   fin si
+   q[j] = qh
+fin pour
+
+exemple :
+B = 10
+u = 256
+v = 127
+
+v[n - 1] = le plus grand digit significatif de v
+// Multiplier u et v par d = {b/(v[n-1]+1]}
+d = {10 / (1 + 1)} = {10 / 2} = {5} = 5
+
+u = u * d = 256 * 5 = 1280
+v = v * d = 127 * 5 = 635
+
+// Le but est que v[n-1] soit plus grand ou égal à B/2 à la fin
+(v[n - 1] = 6) <= (10 / 2)
+//cette etape est reussie
+
+
+//	u = 01280
+//	v = 0635
+//	n = len(v) = 3
+//	m + n = len(u) = 4
+//	m = len(n1) - len(n2) = (len(u) - n) = ((m + n) - n) = (4 - 3) = 1
+
+//	j = m = (len(u) - len(v)) = (4 - 3) = 1
+//Pour j de m à 0 par pas de -1
+
+
+j = 1
+tant que j > 0
+{
+
+	j = 1
+	//qh = {(u[j + n] * B + u[j + n - 1]) / v[n - 1]}
+	//qh = {(u[1 + 3] * B + u[1 + 2]) / v[3 - 1]}
+	//qh = {(u[4] * B + u[3]) / v[2]}
+	//qh = {(0 * B + 1) / 6}
+	//qh = {1 / 6}
+	qh = 0
+
+	//rh = (u[j + n] * B + u[j + n - 1]) % v[n - 1]
+	//rh = (u[1 + 3] * B + u[1 + 3 - 1]) % v[3 - 1]
+	//rh = (u[3] * B + u[3]) % v[2]
+	//rh = (0 * B + 1) % 6
+	rh = 1
+
+	//Si qh = B ou qh * v[n - 2] > rh * B + u[j + n - 2]
+	//if (qh == B || (qh * v[3 - 2] > rh * B + u[1 + 3 - 2]))
+	//if (qh == B || (qh * v[1] > rh * B + u[2]))
+	//if (qh == B || (qh * 3 > rh * B + 2))
+	//if (0 == B || (0 * 3 > 1 * B + 2))
+	//if (0 == 10 || (0 > (1 * 10 + 2)))
+	if (0 == 10 || (0 > 12))
+		//ON ENTRE PAS
+
+	//u = u - qh * v * B^j
+	//u = 1280 - 0 * 635 * B^1
+	u = 1280
+
+	//si u < 0
+	//if (u < 0)
+	if (1280 < 0)
+		//ON ENTRE PAS
+
+	//q[j] = qh
+	q[1] = 0
+
+	j--;
+
+	j = 0
+	//qh = {(u[j + n] * B + u[j + n - 1]) / v[n - 1]}
+	//qh = {12 / 6}
+	qh = 2
+
+	//rh = (u[j + n] * B + u[j + n - 1]) % v[n - 1]
+	//rh = 12 % 2
+	rh = 0
+
+	//if (qh = B || (qh * v[n - 2] > rh * B + u[j + n - 2]))
+	//if (2 == 10 || (2 * 3 > 8))
+	if (2 == 10 || 6 > 8)
+		//ON ENTRE PAS
+
+	//u = u - qh * v * B^j
+	//u = 1280 - 2 * 635 * 10^0
+	//u = 1280 - 1270
+	u = 10
+
+	//if (u < 0)
+	if (10 < 0)
+		//ON ENTRE PAS
+
+	//q[j] = qh
+	q[0] = 2
+}
+
+q = "02"
+
+
+exemple 2:
+u = 1234
+v = 45
+
+v[n - 1] = le plus grand digit significatif
+// Multiplier u et v par d = {b/(v[n-1]+1]}
+d = {10 / (4 + 1)} = {10 / 5} = {2} = 2
+
+u = u * d = 1234 * 2 = 2468
+v = v * d = 45 * 2 = 90
+
+// Le but est que v[n-1] soit plus grand ou égal à B/2 à la fin
+(v[n - 1] = 9) <= (10 / 2)
+cette etape est reussie
+
+*/
+
+# define B (((unsigned long)MAX_BINT_VALS) + 1)
+
+void	bidiv_knuth(t_bint *res, const t_bint *n1, const t_bint *n2)
+{
+	int	qh;
+	int	rh;
+	int	n;
+	int	m;
+	int	j;
+	t_bint	u;
+	t_bint	v
+
+	u = *n1;
+	v = *n2;
+	// etape de normalisation
+	n = v->len;
+	j = u->len - n;
+	while (j >= 0)
+	{
+		//qh = {(u[j + n] * B + u[j + n - 1]) / v[n - 1]}
+		qh = ((u->blks[j + u] * B + u->blks[j + n - 1]) / v->blks[n - 1]);
+		//rh = (u[j + n] * B + u[j + n - 1]) % v[n - 1]
+		rh = ((u->blks[j + u] * B + u->blks[j + n - 1]) % v->blks[n - 1])
+
+		//Si qh = B ou qh * v[n - 2] > rh * B + u[j + n - 2]
+		if (qh == B || (qh * v->blks[n - 2] > (rh * B + u->blks[j + n - 2])))
+		{
+			//qh = qh - 1
+			qh--;
+			//rh = rh + v[n - 1]
+			rh = rh + v->blks[n - 1];
+			//si rh < B et qh * v[n - 2] > rh * B + u[j + n - 2]
+			if (rh < B && (qh * v->blks[n - 2] > rh * B + u->blks[j + n - 2]))
+			{
+				//qh = qh -1
+				qh--;
+				//rh = rh + v[n - 1]
+				rh = rh + v->blks[n - 1];
+			}
+			//fin si
+		}
+		//fin si
+		//u = u - qh * v * B^j
+		u = u - qh * v * B^j;
+
+		//operaton sur bigint
+		bisub(&u, &u, &(qh * B^j * v)) // a modifier pour eviter le if suivant
+
+		//si u < 0
+		if (u < 0) //impossible avec mes bigint. je dois trouver un moyen d'eviter ce cas
+		{
+			//u = u + v
+			biadd(&u, &u, &v);
+			//qh = qh - 1
+			qh--;
+		}
+		//fin si
+		//q[j] = qh
+		q.blks[j--] = qh;
+	}
 }
 
 unsigned int	bidiv10(t_bint *res, t_bint *n1)
